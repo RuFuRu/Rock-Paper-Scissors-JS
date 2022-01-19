a8634fb88a5763256f54f3bb56f8c8aaa3c44033
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-01-19 15:51:30 +0100

    wrote function computerPlay and a prompt

---
+++ script.js
@@ -0,0 +1,8 @@
+// asks for Rock , Paper or Scissors
+let input = prompt("Rock , Paper or Scissors ?");
+
+// function that on user input randomly selects rock,paper or scissor
+// and returns the outcome
+function computerPlay() {
+
+}
