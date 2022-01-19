55697a47bc1b3e0a74dfbb01374576fa6cfa38a7
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-01-19 15:59:21 +0100

    added parseInt to input + array in function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // asks for Rock , Paper or Scissors
-let input = prompt("Rock , Paper or Scissors ?");
+let input = parseInt(prompt("Rock(type in 0) , Paper(type in 1) or Scissors(type in 2) ?"));
 
 // function that on user input randomly selects rock,paper or scissor
 // and returns the outcome
-function computerPlay() {
-
+function computerPlay(input) {
+    //put rock , paper and scissors in an array, then
+    //with math.random select randomly one of those
+    const array = ["Rock", "Paper", "Scissors"];
 }
