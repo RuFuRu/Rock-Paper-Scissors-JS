2b278ec9d574b0d6f0e308fe9bfbd3b56d2f11c0
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-01-19 16:05:05 +0100

    added case insensitivity

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // asks for Rock , Paper or Scissors
-let input = parseInt(prompt("Rock(type in 0) , Paper(type in 1) or Scissors(type in 2) ?"));
+let input = toLowerCase(prompt("Rock , Paper or Scissors?"));
 
 // function that on user input randomly selects rock,paper or scissor
 // and returns the outcome
-function computerPlay(input) {
+function computerPlay() {
     //put rock , paper and scissors in an array, then
     //with math.random select randomly one of those
     const array = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * 3);
-    return console.log(array[random]);
+    return console.log(array[random]); 
 }
