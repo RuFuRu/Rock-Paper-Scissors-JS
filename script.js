1eccec7dfe39ae4aab4ff65a93d1b744cc084921
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-01-19 16:22:48 +0100

    Initialized a function that plays a single round

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-// asks for Rock , Paper or Scissors
-let input = toLowerCase(prompt("Rock , Paper or Scissors?"));
-
 // function that on user input randomly selects rock,paper or scissor
 // and returns the outcome
 function computerPlay() {
     //put rock , paper and scissors in an array, then
     //with math.random select randomly one of those
-    const array = ["Rock", "Paper", "Scissors"];
+    const array = ["rock", "paper", "scissors"];
     let random = Math.floor(Math.random() * 3);
-    return console.log(array[random]); 
+    return array[random]; 
+}
+//write a function that plays a single game of Rock, Paper, Scissors
+// it takes 2 parametres = playerSelection and computerSelection
+//(they should be caseinsensitive)
+//then returns a string "You win" or "You lose" , depending on who
+// won.
+
+// asks for Rock , Paper or Scissors, from the user
+let playerSelection = toLowerCase(prompt("Rock , Paper or Scissors?"));
+let computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    //compare what player and computer selected.
+    //write win or lose based on the following criteria:
+    // rock & scissors = rock win, rock & paper = paper win
+    // rock & rock = draw, scissors & scissors = draw, paper & paper = draw
+    // scissors & paper = scissors win
+    
 }
